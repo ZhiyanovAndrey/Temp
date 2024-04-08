53f7b7546d6d8e02912b03a34216844e279094b1
ZhiyanovAndrey <aazhiyanov@gmail.com>
2024-04-08 11:06:48 +0300

    add class cod review

---
+++ CodReview.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+
+namespace Temp
+{
+    internal class CodReview
+    {
+
+    }
+
+ 
+// Задача: убрать дублирование кода в методах подсчета
+
+class Rectangle
+    {
+        public int CalculatePerimeter()
+        {
+            try
+            {
+                Size size = GetCurrentSize();
+                int result = size.Width + size.Height;
+                LogCalculationSucces();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                LogCalculationError(ex);
+                throw;
+            }
+        }
+        public int CalculateArea()
+        {
+            try
+            {
+                Size size = GetCurrentSize();
+                int result = size.Width * size.Height;
+                LogCalculationSucces();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                LogCalculationError(ex);
+                throw;
+            }
+        }
+
+
+        private Size GetCurrentSize()
+        {
+            throw new NotImplementedException();
+        }
+        private void LogCalculationSucces()
+        {
+            throw new NotImplementedException();
+        }
+        private void LogCalculationError(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
