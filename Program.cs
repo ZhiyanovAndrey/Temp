2450d599d66ca44d6ab3fc34b86557269fbc6172
Andrey <aazhiyanov@gmail.com>
2024-04-08 15:52:23 +0300

    add helper

--- Program.cs
+++ Program.cs
@@ -8,3 +8,30 @@ Console.WriteLine(s1==s2);
 Console.ReadKey();
 
 //assertThat(s1 == s2).isTrue();
+
+
+
+// ввыводит 10, при добавлении ref 15
+
+var a = new A { Value = 5 };
+Helper.Test(a);
+Console.WriteLine(a.Value);
+
+Console.ReadKey();
+
+public class A
+{
+    public int Value { get; set; }
+}
+
+
+public static class Helper
+{
+    public static void Test(A a)
+    {
+        a.Value = 10;
+
+        var b = ReferenceEquals(a.Value, a);
+        a = new A { Value = 15 };
+    }
+}
