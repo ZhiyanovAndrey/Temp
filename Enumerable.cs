ffe0aa521865e72e55970ed54956b61ca6cf4ecc
Andrey <aazhiyanov@gmail.com>
2024-04-15 17:10:25 +0300

    add ConvertToFooBar

---
+++ Enumerable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    public class Enumerable
+    {
+
+        //Input Длинная последовательность чисел.
+        //Output (для одного числа ровно одна строка)
+        //Если делится на 3, то "foo"
+        //Если делится на 5, то "bar"
+        //Если делится на 15, то "foobar"
+        //В противном случае само число, преобразованное в строку
+
+        //ВНИМАНИЕ: В задаче нет никаких подстав и попыток обмануть, напишите решение в лоб
+        //Если вы решаете эту задачу не в IDE — можете использовать однобуквенные имена переменных.
+        //Опечатки, ошибки в синтакисе и вообще все, что подскажет IDE — значения не имеют.
+
+        public static IEnumerable<string> ConvertToFooBar(IEnumerable<int> enumerable)
+        {
+            //if (enumerable == null)
+
+            //    throw reference Exeption;
+
+
+
+            return enumerable.Where(x => x % 3 == 0).Select(x => "foo");
+            //return enumerable.Where(x => x % 5 == 0).Select(x => "bar");
+            //return enumerable.Where(x => x % 15 == 0);
+            //return enumerable.Where(x => x % 3 != 0 && x=>x % 5 != 0 $$ x => x % 15 != 0).Select(x => x.ToString());
+
+
+
+
+            //Foreach(int a in enumerable){
+            //    if (enumerable % 3 == 0) yield return "foo";
+            //    if (enumerable % 5 == 0) yield return "bar";
+            //    if (enumerable % 15 == 0) yield return "foobar";
+            //    else yield return a.ToString();
+            //}
+        }
+
+    }
+
+    //static void Main()
+    //{
+    //    var x = new[] { 3, 7, 15, 5 };
+
+
+    //    //IEnumerable<string> x = null;
+
+
+    //    foreach (var s in ConvertToFooBar(x))
+    //    {
+    //        Console.WriteLine(s);
+    //        // "foo"
+    //        // "7"
+    //        // "foobar"
+    //        // "bar"
+    //    }
+    //}
+
+}
+
--- Program.cs
+++ Program.cs
@@ -2,39 +2,54 @@
 
 
 
+var x = new[] { 3, 7, 15, 5 };
+
+
+//IEnumerable<string> x = null;
+
+
+foreach (var s in ConvertToFooBar(x))
+{
+    Console.WriteLine(s);
+    // "foo"
+    // "7"
+    // "foobar"
+    // "bar"
+}
+
+static IEnumerable<string> ConvertToFooBar(IEnumerable<int> enumerable)
+{
+    //if (enumerable == null)
+
+    //    throw reference Exeption;
+
+
+
+    return enumerable.Where(x => x % 3 == 0).Select(x => "foo");
+    //return enumerable.Where(x => x % 5 == 0).Select(x => "bar");
+    //return enumerable.Where(x => x % 15 == 0);
+    //return enumerable.Where(x => x % 3 != 0 && x=>x % 5 != 0 $$ x => x % 15 != 0).Select(x => x.ToString());
+
+
+
+
+    //Foreach(int a in enumerable){
+    //    if (enumerable % 3 == 0) yield return "foo";
+    //    if (enumerable % 5 == 0) yield return "bar";
+    //    if (enumerable % 15 == 0) yield return "foobar";
+    //    else yield return a.ToString();
+    //}
+}
+
 //String s1 = "Hello World";
 //String s2 = "Hello World";
 
 //Console.WriteLine(s1==s2);
 //Console.ReadKey();
 
-//assertThat(s1 == s2).isTrue();
 
 
 
-// ввыводит 10, при добавлении ref выводит 15
-
-
-//string num = "2";
-
-//Multiply(ref num); // не изменяет num без ref
-
-//Console.WriteLine($"num {num}");
-
-
-int i = 123;
-object o = i;  // implicit boxing
-
-try
-{
-    int j = (short)o;  // attempt to unbox
-
-    System.Console.WriteLine("Unboxing OK.");
-}
-catch (Exception e)
-{
-    System.Console.WriteLine("{0} Error: Incorrect unboxing.", e.Message);
-}
 
 //// приммер 1
 int i1 = 1;
@@ -58,9 +73,10 @@ System.Console.WriteLine("o = {0}", o2); // 123
 
 
 
+// ввыводит 10, при добавлении ref выводит 15
 
-A a = new A { Value = 5 };  // создаем объект ссылочный, что в фигурных
-Helper.Test(a); // передаем ссылку на 5
+A a = new A { Value = 5 };  // создаем объект ссылочный, что за запись 1 
+Helper.Test(a); // копируем ссылку на a
 Console.WriteLine(a.Value); // 10
 Console.WriteLine(a.ToString()); // A
 Console.WriteLine(a.GetType()); // A ????
@@ -69,31 +85,6 @@ Console.WriteLine();
 
 
 Console.ReadKey();
-
-
-static void Multiply(ref string i) // будет создана новая переменная (их называют локальными)
-{
-    Console.WriteLine($"Multiply {i + i}"); // выведет 4 не изменит i
-    i += i; // изменит локальную переменную
-}
-
-
-static void AnonymousType()
-{
-    var v = new { Title = "Hello", Age = 24 };
-
-    Console.WriteLine(v.ToString()); // "{ Title = Hello, Age = 24 }"
-}
-
-static void AnonymousTypeMutation()
-{
-    var apple = new { Item = "apples", Price = 1.35 };
-    var onSale = apple with { Price = 0.79 };
-    Console.WriteLine(apple); // "{ Item = "apples", Age = 24 }"
-    Console.WriteLine(onSale); // "{ Item = "apples", Age = 24 }"
-}
-
-
 
 
 
@@ -105,11 +96,44 @@ public class A
 
 public static class Helper
 {
-    public static void Test(A a)
+    public static void Test(A a) // 2 ссылка
     {
-        a.Value = 10;
+        a.Value = 10; // изменяем локальную переменную a и глобальная переменная тоже меняется 2
 
-        var b = ReferenceEquals(a.Value, a);
-        a = new A { Value = 15 };
+        a = new A { Value = 15 }; // изменяет только локальную переменную другая ссылка
     }
 }
+
+
+//string num = "2";
+
+//Multiply(ref num); // не изменяет num без ref
+
+//Console.WriteLine($"num {num}");
+
+//static void Multiply(ref string i) // будет создана новая переменная (их называют локальными)
+//{
+//    Console.WriteLine($"Multiply {i + i}"); // выведет 4 не изменит i
+//    i += i; // изменит локальную переменную
+//}
+
+
+//static void AnonymousType()
+//{
+//    var v = new { Title = "Hello", Age = 24 };
+
+//    Console.WriteLine(v.ToString()); // "{ Title = Hello, Age = 24 }"
+//}
+
+//static void AnonymousTypeMutation()
+//{
+//    var apple = new { Item = "apples", Price = 1.35 };
+//    var onSale = apple with { Price = 0.79 };
+//    Console.WriteLine(apple); // "{ Item = "apples", Age = 24 }"
+//    Console.WriteLine(onSale); // "{ Item = "apples", Age = 24 }"
+//}
+
+
+
+
+
