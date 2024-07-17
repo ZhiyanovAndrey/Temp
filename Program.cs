034e4219b82ce29e65073d5103bcd0fc402a0193
ZhiyanovAndrey <aazhiyanov@gmail.com>
2024-07-17 10:21:10 +0300

    add remarks

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@
 Firm f = new Firm();
 Console.WriteLine(f.INN);  //1 /null
 f.INN = 3;
-//ChangeINN(f);
-Console.WriteLine(f.INN); //3
+Firm.ChangeINN(ref f); // изменяет переменную на 2
+Console.WriteLine(f.INN); //2
 
 
 Console.ReadKey();
@@ -17,7 +17,7 @@ public class Firm
 {
     public int INN = 1; // при обьявлении класса поле инициализируется inn 1
 
-    public static void ChangeINN(Firm firm)
+    public static void ChangeINN(ref Firm firm)
     {
         firm.INN = 2;
         firm = new Firm();
