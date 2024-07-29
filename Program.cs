8248f748fff621d26a5cad633112e9eff66f3f96
Andrey <aazhiyanov@gmail.com>
2024-07-29 09:07:41 +0300

    work with Classes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
 Firm f = new Firm();
 Console.WriteLine(f.INN);  //1 /null
 f.INN = 3;
-Firm.ChangeINN(ref f); // изменяет переменную на 2
+Firm.ChangeINN(f); // изменяет переменную на 2, если с реф то не изменит
 Console.WriteLine(f.INN); //2
 
 
@@ -17,13 +17,36 @@ public class Firm
 {
     public int INN = 1; // при обьявлении класса поле инициализируется inn 1
 
-    public static void ChangeINN(ref Firm firm)
+    public static void ChangeINN(Firm firm)  // если с ref то 1
     {
         firm.INN = 2;
         firm = new Firm();
     }
 
 }
+
+
+//var a = new A { Value = 5 };
+//Helper.Test(ref a);
+//Console.WriteLine(a.Value); // выведет 10 с ref 15
+
+//Console.ReadKey();
+
+//public class A
+//{
+//    public int Value { get; set; }
+//}
+
+
+//public static class Helper
+//{
+//    public static void Test(ref A a)
+//    {
+//        a.Value = 10;
+
+//        a = new A { Value = 15 };
+//    }
+//}
 
 
 
